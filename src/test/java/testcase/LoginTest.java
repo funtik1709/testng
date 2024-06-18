af2398c644ff815dd28af1e49cd23029e1b7c04c
e.nurmaganbetov <e.nurmaganbetov@ddirection.kz>
2024-06-18 14:29:27 +0600

    user login test

--- src/test/java/testcase/LoginTest.java
+++ src/test/java/testcase/LoginTest.java
@@ -1,34 +1,27 @@
 package testcase;
 
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
-public class LoginTest {
+import base.BaseTest;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver/chromedriver.exe");
 
-		WebDriver driver = new ChromeDriver();
 
-		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-		driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
-		driver.manage().timeouts().setScriptTimeout(15, TimeUnit.SECONDS);
-
-		driver.manage().window().maximize();
-
-		driver.get("https://tekmetest.ddirection.kz/");
+public class LoginTest extends BaseTest {
+	@Test
+	public static void  Login() throws InterruptedException {
 		
-		driver.findElement(By.xpath("//*[@id=\"__nuxt\"]/div/header/div[1]/nav/div[2]/div[2]/a/span")).click();
+		driver.findElement(By.xpath("//*[text()='Личный кабинет']")).click();
+		Thread.sleep(3000);
 		
-		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("citaf36963@acuxi.com");
-		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("Aa12345678");
-		
+		driver.findElement(By.xpath("//input[@id='email']")).sendKeys(prop.getProperty("login_email"));
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys(prop.getProperty("login_password"));
+		Thread.sleep(3000);
 		driver.findElement(By.xpath("//*[@id=\"modal1\"]/div/form/div[3]/button")).click();
 		
-	}
+	}	
+		
+		
+	
 
 }
--- src/test/java/testcase/MyFirsTestFW.java
+++ src/test/java/testcase/MyFirsTestFW.java
@@ -12,14 +12,90 @@ import base.BaseTest;
 public class MyFirsTestFW extends BaseTest {
 	
 	@Test
-	public static void  RegisterTest()
+	public static void  RegisterTest() throws InterruptedException
 	{
 		
 			
-		 WebElement link_doctors = driver.findElement(By.xpath("//*[@id=\"__nuxt\"]/div/header/div[1]/nav/ul/li[2]/a"));
+		 WebElement link_doctors = driver.findElement(By.xpath("//*[text()='Для врачей']"));
 	     link_doctors.click();
+	     
+	     Thread.sleep(3000);
+	     
+	     WebElement link_order = driver.findElement(By.xpath("//*[text()='Подать заявку']"));
+	        link_order.click();
 	        
-		
+	        
+	     // FILL THE REGISTER FORM
+
+	        //first_name
+	        WebElement first_name = driver.findElement(By.xpath("//*[@id=\"name\"]"));
+	        first_name.sendKeys("Иван");
+
+	        //last_name 
+	        WebElement last_name = driver.findElement(By.xpath("//*[@id=\"surname\"]"));
+	        last_name.sendKeys("Иванов");
+
+	        //middle_name
+	        WebElement middle_name = driver.findElement(By.xpath("//*[@id=\"patronymic\"]"));
+	        middle_name.sendKeys("Иванович");
+
+	        //select_sex
+	        Select select_sex = 
+	        new Select
+	        (driver.findElement
+	        (By.xpath("//*[@id=\"sex\"]")));
+	        select_sex.selectByVisibleText("Мужской");
+
+	        //iin
+	        WebElement  iin = driver.findElement(By.xpath("//*[@id=\"iin\"]"));
+	        iin.sendKeys("850527361128");
+
+	        //phone
+	        WebElement  phone = driver.findElement(By.xpath("//*[@id=\"phone\"]"));
+	        phone.sendKeys("+77474564850");
+
+	        //email
+	        WebElement  email = driver.findElement(By.xpath("//*[@id=\"email\"]"));
+	        email.sendKeys(prop.getProperty("reg_email"));
+
+	        //b_date
+	        WebElement  b_date = driver.findElement(By.xpath("//*[@id=\"date_birthdate\"]"));
+	        b_date.sendKeys("19-04-1987");
+
+	        //select_region
+	        Select select_region = new Select(driver.findElement(By.xpath("//*[@id=\"area_id\"]")));
+	        select_region.selectByVisibleText("Карагандинская область");
+
+	        //select_city
+	        Select select_city = new Select(driver.findElement(By.xpath("//*[@id=\"city_id\"]")));
+	        select_city.selectByVisibleText("Караганда");
+
+	        //agree
+	        WebElement checkbox = driver.findElement(By.id("agree"));
+	        checkbox.click();
+
+	        // submit
+	        WebElement submit = driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div/form/div[12]/button"));
+	        submit.click();
+	        Thread.sleep(2000);
+
+	        // FILL THE FILES FORM	        
+
+	        // pdf test
+	        WebElement id_file = driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div/form/div[1]/input"));
+	        id_file.sendKeys("C:\\Users\\User\\Pictures\\test\\21.pdf");
+	        Thread.sleep(2000);
+	        
+	        WebElement diplom_file = driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div/form/div[2]/input"));
+	        diplom_file.sendKeys("C:\\Users\\User\\Pictures\\test\\22.pdf");
+	        Thread.sleep(2000);
+	        
+	        WebElement cert_file = driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div/form/div[3]/input"));
+	        cert_file.sendKeys("C:\\Users\\User\\Pictures\\test\\23.pdf");
+	        Thread.sleep(2000);
+	        //submit_files
+	        WebElement submit_files = driver.findElement(By.xpath("/html/body/div[1]/div/div[2]/div/form/div[4]/button"));
+	        submit_files.click();   
 		
 	}
 	
