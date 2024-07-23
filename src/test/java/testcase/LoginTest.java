b737e46693c29439cd98f48b0e1d030fa1848c1e
e.nurmaganbetov <e.nurmaganbetov@ddirection.kz>
2024-07-23 10:16:29 +0600

    read xls file completed

--- src/test/java/testcase/LoginTest.java
+++ src/test/java/testcase/LoginTest.java
@@ -5,38 +5,30 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import base.BaseTest;
-
-
+import utilities.ReadXLSdata;
 
 public class LoginTest extends BaseTest {
-	@Test(dataProvider="testdata")
-	public static void  Login(String username, String password) throws InterruptedException {
+	@Test(dataProviderClass=ReadXLSdata.class, dataProvider="bvtdata")
+	public static void Login(String username, String password) throws InterruptedException {
 		driver.manage().window().maximize();
-		
+
 		driver.findElement(By.xpath(loc.getProperty("signin_link"))).click();
 		Thread.sleep(3000);
 		System.out.println(loc.getProperty("signin_link"));
-		
-		//data provider
+
+		// data provider
 		driver.findElement(By.id(loc.getProperty("email_field"))).sendKeys(username);
 		driver.findElement(By.id(loc.getProperty("password_field"))).sendKeys(password);
-		
-		//from config file
+
+		// from config file
 //		driver.findElement(By.id(loc.getProperty("email_field"))).sendKeys(prop.getProperty("login_email"));
 //		driver.findElement(By.id(loc.getProperty("password_field"))).sendKeys(prop.getProperty("login_password"));
 		Thread.sleep(3000);
-		driver.findElement(By.xpath(loc.getProperty("login_button"))).click();		
-		
-	}	
-		
-	@DataProvider(name="testdata")
-	public Object[][] tData()
-	{
-		return new Object[][] {
-			{"citaf36963@acuxi.com", "Aa12345678"},
-			{"pehhtr@ad.ri", "Aa12345678"},
-			{"erkejan.amanjolovna@gmail.com", "Qwerty123"}
-		};
+		driver.findElement(By.xpath(loc.getProperty("login_button"))).click();
+
 	}
+
+	
+	
 
 }
